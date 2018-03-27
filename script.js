37ffbe69b4ecd3916f3c5d3224a415c4ff5bf326
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-03-27 16:04:58 -0400

    added to css, html and array in js

--- script.js
+++ script.js
@@ -5,10 +5,16 @@
 // for loop will loop through the questions once
 //   store the 5 correct answers in an array
 
+// link the corresponding form event listener function to the correct answer
+
 // Loop through previous step four more times
 //
 // Let player see the number of answers they got correct
 // Store correct answer in an array or in html
-document.querySelector('#first-question').addEventListener('change', function (event) {
+    let answersToQuiz = [0, 1, 2, 3, 4]
+    console.log(answersToQuiz, 0)
+
+    
+    document.querySelector('#first-question').addEventListener('change', function (event) {
     console.log(event.target.value)
 })
