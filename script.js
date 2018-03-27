8bc73954767e1c3c8a8867dbc4c3d26ea80f58a0
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-03-27 15:03:48 -0400

    added to js and updated pseudocode

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Player opens page to the first question
 // Let player see question
-/* Let player choose between a series of answers using for loops
-for loop will loop through the questions five times */
-/* Loop through previous step four more times
-*/
+// Let player choose between a series of answers using for loops
+
+// for loop will loop through the questions once
+//   store the 5 correct answers in an array
+
+// Loop through previous step four more times
+//
 // Let player see the number of answers they got correct
 // Store correct answer in an array or in html
+document.querySelector('#first-question').addEventListener('change', function (event) {
+    console.log(event.target.value)
+})
