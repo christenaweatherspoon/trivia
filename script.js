db0a9f9195ceda182dd8453a271c0efe82744aae
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-03-27 11:17:17 -0400

    updated  pseudocode added to comments

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 // Let player see question
 /* Let player choose between a series of answers using for loops
 for loop will loop through the questions five times */
-
 /* Loop through previous step four more times
 */
-
 // Let player see the number of answers they got correct
+// Store correct answer in an array or in html
