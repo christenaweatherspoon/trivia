a48a7517a2a0b296ec840d9ebb7d73c0634e73e3
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-03-27 11:03:00 -0400

    some pseudocode added to comments

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Player opens page to the first question
 // Let player see question
-// Let player choose between a series of answers
-// Loop through previous step four more times
+/* Let player choose between a series of answers using for loops
+for loop will loop through the questions five times */
+
+/* Loop through previous step four more times
+*/
+
 // Let player see the number of answers they got correct
