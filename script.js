6750aaf897c8a779f2ff5bd2923f66e582316fa9
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-03-27 10:47:58 -0400

    comments revised

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-//Player opens page to the first question
-
-
-//Player is presented with a choice of questions, only one is the correct answer
-
-
-//Player goes to the second question
-
-
-//Player goes to the third question
-
-
-//Player goes to the fourth question
-
-
-//Player goes tothe fifth question
-
-
-//Player is presented with a page that shows how many questions they got right and wrong
-
-
-//If all goes well I will add a score in the top right corner and get rid of the score page
+// Player opens page to the first question
+// Let player see question
+// Let player choose between a series of answers
+// Loop through previous step four more times
+// Let player see the number of answers they got correct
