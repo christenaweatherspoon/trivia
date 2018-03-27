49fda40af368e838185a69c0e304ab7303ac4ddc
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-03-27 10:34:40 -0400

    comments added

---
+++ script.js
@@ -0,0 +1,22 @@
+//Player opens page to the first question
+
+
+//Player is presented with a choice of questions, only one is the correct answer
+
+
+//Player goes to the second question
+
+
+//Player goes to the third question
+
+
+//Player goes to the fourth question
+
+
+//Player goes tothe fifth question
+
+
+//Player is presented with a page that shows how many questions they got right and wrong
+
+
+//If all goes well I will add a score in the top right corner and get rid of the score page
