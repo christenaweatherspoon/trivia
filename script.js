61a4ca41757aee3e37476b209b9af4528803784a
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-03-28 09:23:16 -0400

    array

--- script.js
+++ script.js
@@ -24,17 +24,17 @@ document
 
 // maybe create a variable that says the question and then a seperate one with the answer
     triviaQuestionOne = [
-      ['Radric Delantic Davis', 'Nayvadius DeMun Wilburn', 'Jeffery Lamar Williams', 'DeAndre Cortez Way'],
+      ['Radric Delantic Davis.', 'Nayvadius DeMun Wilburn.', 'Jeffery Lamar Williams.', 'DeAndre Cortez Way.'],
       
       triviaQuestionTwo = 
-      ['Miami, Florida', 'Youngstown, Ohio', 'Bessemer, Alabama', 'Atlanta, Georgia'],
+      ['Miami, Florida.', 'Youngstown, Ohio.', 'Bessemer, Alabama.', 'Atlanta, Georgia.'],
       
       triviaQuestionThree = 
-      ['It’s October, 17th', 'He actually borrowed the number from the address of his grandfathers house', 'It’s the time his son was born', '1017 is the last four digits of is social security number'],
+      ['It’s October, 17th.', 'He actually borrowed the number from the address of his grandfathers house.', 'It’s the time his son was born.', '1017 is the last four digits of is social security number.'],
   
       triviaQuestionFour = 
-      ['Yes', 'No', 'Maybe so', 'Bitch, I might be'],
+      ['Yes.', 'No.', 'Maybe so.', 'Bitch, I might be.'],
       
       triviaQuestionFive = 
       
-      ['A portrait of his grandfather', 'A Smith & Wesson M&P 380', 'His wifes name', 'An ice cream cone']
+      ['A portrait of his grandfather.', 'A Smith & Wesson M&P 380.', 'His wifes name.', 'An ice cream cone.']
