d17e725c591b77d28160e107f91706e43aed485a
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-03-28 11:09:08 -0400

    formatted code

--- script.js
+++ script.js
@@ -11,22 +11,68 @@
 //
 // Let player see the number of answers they got correct
 // Store correct answer in an array or in html
+
 // maybe create a variable that says the question and then a seperate one with the answer
-  
+let answersToQuestionOne = [
+  'Radric Delantic Davis.',
+  'Nayvadius DeMun Wilburn.',
+  'Jeffery Lamar Williams.',
+  'DeAndre Cortez Way.'
+];
 
-let answersToQuestionOne = ['Radric Delantic Davis.', 'Nayvadius DeMun Wilburn.', 'Jeffery Lamar Williams.', 'DeAndre Cortez Way.']
-      
-let answersToQuestionTwo = ['Miami, Florida.', 'Youngstown, Ohio.', 'Bessemer, Alabama.', 'Atlanta, Georgia.']
-      
-let answersToQuestionThree = ['It’s October, 17th.', 'He actually borrowed the number from the address of his grandfathers house.', 'It’s the time his son was born.', '1017 is the last four digits of is social security number.']
-  
-let answersToQuestionFour = ['Yes.', 'No.', 'Maybe so.', 'Bitch, I might be.']
-     
-let answersToQuestionFive = ['A portrait of his grandfather.', 'A Smith & Wesson M&P 380.', 'His wifes name.', 'An ice cream cone.']
+let answersToQuestionTwo = [
+  'Miami, Florida.',
+  'Youngstown, Ohio.',
+  'Bessemer, Alabama.',
+  'Atlanta, Georgia.'
+];
 
+let answersToQuestionThree = [
+  'It’s October, 17th.',
+  'He actually borrowed the number from the address of his grandfathers house.',
+  'It’s the time his son was born.',
+  '1017 is the last four digits of is social security number.'
+];
 
-let answersToQuiz = ['Radric Delantic Davis.', 'Atlanta, Georgia.', 'He actually borrowed the number from the address of his grandfathers house.', 'Bitch, I might be.', 'An ice cream cone.']
+let answersToQuestionFour = ['Yes.', 'No.', 'Maybe so.', 'Bitch, I might be.'];
 
-for (let i = 0; i < triviaQuiestions.length; i++) {
-  console.log(answersToQuiz[i])
-}
+let answersToQuestionFive = [
+  'A portrait of his grandfather.',
+  'A Smith & Wesson M&P 380.',
+  'His wifes name.',
+  'An ice cream cone.'
+];
+
+let answersToQuiz = [
+  [
+    'Radric Delantic Davis.',
+    'Nayvadius DeMun Wilburn.',
+    'Jeffery Lamar Williams.',
+    'DeAndre Cortez Way.'
+  ],
+  [
+    'Miami, Florida.',
+    'Youngstown, Ohio.',
+    'Bessemer, Alabama.',
+    'Atlanta, Georgia.'
+  ],
+  [
+    'It’s October, 17th.',
+    'He actually borrowed the number from the address of his grandfathers house.',
+    'It’s the time his son was born.',
+    '1017 is the last four digits of is social security number.'
+  ],
+  ['Yes.', 'No.', 'Maybe so.', 'Bitch, I might be.'],
+  [
+    'A portrait of his grandfather.',
+    'A Smith & Wesson M&P 380.',
+    'His wifes name.',
+    'An ice cream cone.'
+  ]
+];
+console.log(answersToQuiz, 0);
+document
+  .querySelector('#first-question')
+  .addEventListener('change', function(event) {
+    console.log(event.target.value);
+  });
