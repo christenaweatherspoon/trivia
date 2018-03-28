e0c745f691a91e5d0cc52ead660e601d2c2d78dc
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-03-27 21:30:19 -0400

    array for questions created

--- script.js
+++ script.js
@@ -11,10 +11,29 @@
 //
 // Let player see the number of answers they got correct
 // Store correct answer in an array or in html
-    let answersToQuiz = [0, 1, 2, 3, 4]
-    console.log(answersToQuiz, 0)
-
-    
-    document.querySelector('#first-question').addEventListener('change', function (event) {
+let answersToQuiz = [0, 1, 2, 3, 4]
+console.log(answersToQuiz, 0)
+document
+  .querySelector('#first-question')
+  .addEventListener('change', function (event) {
     console.log(event.target.value)
-})
+  })
+
+
+  let triviaQuiestions =  
+
+    triviaQuestionOne = [
+      ['Radric Delantic Davis', 'Nayvadius DeMun Wilburn', 'Jeffery Lamar Williams', 'DeAndre Cortez Way'],
+      
+      triviaQuestionTwo = 
+      ['Miami, Florida', 'Youngstown, Ohio', 'Bessemer, Alabama', 'Atlanta, Georgia'],
+      
+      triviaQuestionThree = 
+      ['It’s October, 17th', 'He actually borrowed the number from the address of his grandfathers house', 'It’s the time his son was born', '1017 is the last four digits of is social security number'],
+  
+      triviaQuestionFour = 
+      ['Yes', 'No', 'Maybe so', 'Bitch, I might be'],
+      
+      triviaQuestionFive = 
+      
+      ['A portrait of his grandfather', 'A Smith & Wesson M&P 380', 'His wifes name', 'An ice cream cone']
